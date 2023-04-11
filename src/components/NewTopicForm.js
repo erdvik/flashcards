fceb9c1f0aa54dbf65c4cc8334831776aaba37b0
rdv1k <43339320+rdv1k@users.noreply.github.com>
2023-04-11 20:55:52 +0200

    Generate topic from topic form

--- src/components/NewTopicForm.js
+++ src/components/NewTopicForm.js
@@ -4,12 +4,14 @@ import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
 import { ALL_ICONS } from "../data/icons";
 import { addTopic } from "../features/topics/topicsSlice";
+import { useDispatch } from "react-redux";
 
 
 export default function NewTopicForm() {
   const [name, setName] = useState("");
   const [icon, setIcon] = useState("");
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,7 +20,7 @@ export default function NewTopicForm() {
     }
 
     // dispatch your add topic action here
-    
+    dispatch(addTopic({ id: uuidv4(), name, icon }));
     history.push(ROUTES.topicsRoute());
   };
 
