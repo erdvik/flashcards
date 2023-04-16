5d93cb2735116756569814671c2a435c7ef3eb03
rdv1k <43339320+rdv1k@users.noreply.github.com>
2023-04-16 20:24:49 +0200

    Quizzes shown after being added

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -22,7 +22,7 @@ const topicsSlice = createSlice({
     }
 })
 
-export const selectTopics = state => state.topics.topics
+export const selectTopics = state => state.topics.topics;
 export const { addTopic, addQuizId } = topicsSlice.actions
 export default topicsSlice.reducer
 
